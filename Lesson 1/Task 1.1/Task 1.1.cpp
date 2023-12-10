7216a8ae47301d73ad2caaf771a7011508ef08f3
MaksimCPP8 <maksgreat1978@gmail.com>
2023-12-10 22:15:58 +0400

    Added Tasks 1.1, 1.2 to the Lesson 1 folder

---
+++ Lesson 1/Task 1.1/Task 1.1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+thread_local int count=0;
+
+void customer (int value, int &count)
+{    
+    for (auto i = 0; i < value; i++)
+    {
+        std::this_thread::sleep_for(1000ms);
+        count++;
+        std::cout << count << " ";        
+    }
+}
+
+void operater (int value, int &count)
+{    
+    for (auto i = value; i > 0; i--)
+    {
+        std::this_thread::sleep_for(2000ms);
+        count--;
+        std::cout << count << " ";
+    }
+}
+
+int main()
+{
+    setlocale(0, "");
+    
+    std::cout << "\n\t\tЗадача №1. Очередь клиентов.\n\n";
+    
+    int value = 10;
+
+    std::cout << "\nКоличество клиентов на запись: " << value;
+
+    std::cout << "\n\nОчередь клиентов: ";
+
+    std::thread th1(customer, value, std::ref(count));
+    std::this_thread::sleep_for(10ms);
+    std::thread th2(operater, value, std::ref(count));
+
+    th1.join();
+    th2.join();
+
+    std::cout << "\n\nОбработка очереди завершена.\n";
+
+    return 0;
+}
---
+++ Lesson 1/Task 1.2/Task 1.2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <execution>
+#include <random>
+#include <iterator>
+#include <vector>
+
+using namespace std::chrono_literals;
+
+void sum_vector(std::vector <int> vec1, std::vector <int> vec2)
+{ 
+    auto sum = 0;
+    auto start = std::chrono::steady_clock::now();
+    for (auto it : vec1) { sum += it; }
+    for (auto it : vec2) { sum += it; }
+    auto finish = std::chrono::steady_clock::now();
+    auto result = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
+    std::cout << result.count() << "ms\t\t";
+}
+
+int main()
+{
+    setlocale(0, "");
+
+    std::cout << "\n\t\tЗадача №2. Параллельные вычисления.\n\n";
+
+    std::cout << "Колличество аппаратных ядер - " << std::thread::hardware_concurrency() << ".\n";
+
+    std::cout << "\n\n\t\t" << "1 000\t\t" << "10 000\t\t" << "100 000\t\t" << "1 000 000\n";
+
+    for (int thread = 1; thread <= 16; thread *= 2)
+    {
+        std::cout << thread << " потоков\t";
+
+        for (int i = 1; i <= thread; i++)
+        {          
+            for (size_t size = 1000; size <= 1000000; size *= 10)
+            {
+                std::vector<int> vec1(size);
+                std::mt19937 gen1;
+                std::uniform_int_distribution<int> dist1(0, size);
+                std::generate(std::execution::par, vec1.begin(), vec1.end(), [&]() { return dist1(gen1); });
+
+                std::vector<int> vec2(size);
+                std::mt19937 gen2;
+                std::uniform_int_distribution<int> dist2(0, size);
+                std::generate(std::execution::par, vec2.begin(), vec2.end(), [&]() { return dist2(gen2); });
+
+                std::thread th[i](sum_vector, vec1, vec2);
+                th[i].join();
+            }
+            std::cout << std::endl;
+        }
+    }
+    return 0;
+}
